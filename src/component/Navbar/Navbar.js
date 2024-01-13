e834dc209206e809c2c7c4cd4997bc6eaf31fed3
manojconcept <manojveerapavu@gmail.com>
2024-01-13 14:47:21 +0530

    future fix

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -7,29 +7,29 @@ function Navbar() {
         sessionStorage.clear()
         setIslog(false);
     }
+    const handleShow =()=>{
+        setIslog(true)
+    }
     const sessionFlag = sessionStorage.getItem('auth') !== null ? true : false;
     return (
         <>
             <nav style={{ position: "sticky", top: "0", zIndex: "9000" }} className="navbar navbar-dark bg-dark  justify-content-between">
                 <div className="container">
-                    <Link to={"/"}  className="navbar-brand"><i className="bi bi-database"></i>  User Database</Link>
+                    <Link to={"/"} onClick={handleShow}  className="navbar-brand"><i className="bi bi-database"></i>  User Database</Link>
                     {
                         sessionFlag ? <Link to={"/admin"}  path className="navbar-brand mx-2">{`--Hello ${JSON.parse(sessionStorage.getItem("auth")).username}/admin--`}</Link> : ""
 
                     }
 
                     <div className="d-flex">
-                    <button onClick={() => handleLogin("signin")} className="btn btn-light my-2 my-sm-0" > <i className="bi bi-person-fill-lock"></i> Login</button>
+                    <button onClick={() => handleLogin("signin")} className="btn btn-light my-2 my-sm-0" > <i className="bi bi-person-fill-lock"></i> Login</button> 
                         {/* {
                             isLog ? <button onClick={() => handleLogin("signup")} className="btn btn-light my-2 my-sm-0" ><i className="bi bi-person-plus-fill"></i> Signup</button> : <button onClick={() => handleLogin("signin")} className="btn btn-light my-2 my-sm-0" > <i className="bi bi-person-fill-lock"></i> Login</button>
                         } */}
                         {
                             sessionFlag ? <Link to={"/"} onClick={handleClear} className="mx-2 btn btn-danger my-2 my-sm-0">Log out</Link>:""
-
                         }
                     </div>
-
-
                 </div>
             </nav>
         </>
--- src/config/Utils.js
+++ src/config/Utils.js
@@ -1,34 +1,35 @@
-import { useContext, createContext,useEffect,useState } from "react"
-import {useNavigate} from "react-router-dom"
-import { getData } from "./Api";
-import { adminData } from "./Db";
+    import { useContext, createContext,useEffect,useState } from "react"
+    import {useNavigate} from "react-router-dom"
+    import { getData } from "./Api";
+    import { adminData } from "./Db";
 
-const flowDat = createContext();
-export const GobalContext = () => {
-    return useContext(flowDat);
-}
-export const UsersWrapperList = ({ children }) => {
+    const flowDat = createContext();
+    export const GobalContext = () => {
+        return useContext(flowDat);
+    }
+    export const UsersWrapperList = ({ children }) => {
 
-    const [users, setUsers] = useState([]);
-    useEffect(() => { getData(setUsers) }, []); 
-    const [isLog, setIslog] = useState(false);
-    const navigate = useNavigate()
-    const handleLogin = (reDir) => {
-        navigate(`/${reDir}`);
-        setIslog(!isLog);
+        const [users, setUsers] = useState([]);
+        useEffect(() => { getData(setUsers) }, []); 
+        const [isLog, setIslog] = useState(false);
+        const navigate = useNavigate()
+
+        const handleLogin = (reDir) => {
+            navigate(`/${reDir}`);
+            setIslog(false);
+        }
+        const dataFall = {
+            users,
+            setUsers,
+            adminData,
+            isLog,
+            setIslog,
+            handleLogin
+        }
+        return (
+            <flowDat.Provider value={dataFall}>
+                {children}
+            </flowDat.Provider>
+        )
     }
-    const dataFall = {
-        users,
-        setUsers,
-        adminData,
-        isLog,
-        setIslog,
-        handleLogin
-    }
-    return (
-        <flowDat.Provider value={dataFall}>
-            {children}
-        </flowDat.Provider>
-    )
-}
 
